8550475916a252610ea2172c4ef16e5a2037f872
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-08-27 20:27:28 +0200

    sieve

---
+++ prime_numbers/sieve.cpp
@@ -0,0 +1,30 @@
+// sieve of eratosthenes
+
+#include <cmath>
+#include <iostream>
+
+using ll = long long;
+
+int main(void) {
+    ll n;
+    std::cout << "Enter the limit: ";
+    std::cin >> n;
+
+    bool is_prime[n + 1];
+    std::fill(is_prime, is_prime + n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    ll limit = std::sqrt(n);
+    for (int i = 2; i <= limit + 1; i++) {
+        if (!is_prime[i])
+            continue;
+        for (int j = i * i; j <= n; j += i)
+            is_prime[j] = false;
+    }
+
+    for (int i = 0; i <= n; i++) {
+        if (is_prime[i])
+            std::cout << i << '\n';
+    }
+
+    return 0;
+}
